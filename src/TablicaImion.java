5aac8b263cbeeda7be47e31a410dd08b8434cce1
wojga476 <wojga476@gmail.com>
2018-06-07 17:57:45 +0200

    stworzenie tablicy z imionami

---
+++ src/TablicaImion.java
@@ -0,0 +1,13 @@
+public class TablicaImion {
+
+    public static void main(String[] args) {
+        String[] tablica = new String[3];
+        tablica[0]= "Natalia";
+        tablica[1]= "Andrzej";
+        tablica[2]= "Magda";
+
+        System.out.println(tablica[0]);
+        System.out.println(tablica[1]);
+        System.out.println(tablica[2]);
+    }
+}
